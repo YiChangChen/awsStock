b61807a1ea619ed7960f53fdeecd94d55a5f17cc
YiChang Chen <yichangchen@YiChangdeMacBook-Air.local>
2024-10-17 01:30:46 +0800

    commit

---
+++ src/app/services/stock.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Stock, StockInfo } from '../model/stock';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+
+  constructor(private http: HttpClient) { }
+
+  getStock(model: Stock): Observable<StockInfo[]> {
+    const url = 'https://qjeiwpbr5l.execute-api.us-east-1.amazonaws.com/default/twstock-lambda-3-12-4'
+
+    return this.http.post<StockInfo[]>(url, model);
+  }
+}
